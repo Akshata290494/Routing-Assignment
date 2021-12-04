88bdee6968f41800e6f58b9119a1a997b7f5d53e
Akshata290494 <92198619+Akshata290494@users.noreply.github.com>
2021-12-04 17:09:18 +0530

    changes

---
+++ src/component/about.js
@@ -0,0 +1,6 @@
+import React from 'react';
+ 
+function About () {
+    return <h1>Welcome to the About Us Page!</h1>  
+}
+export default About;
---
+++ src/component/contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+ 
+function Contact (){
+ return <address>
+            You can find us here:<br />
+            Company Name :<br />
+            Address <br />
+            
+        </address>
+}
+ 
+export default Contact;
